1e697590b6fff6fe6061735151b5348d933cc1b8
Pankaj <pnkj@email.com>
2025-01-12 17:07:45 +0530

    war file practice

---
+++ src/test/java/com/devops/MyHelloProject/HelloProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.MyHelloProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
